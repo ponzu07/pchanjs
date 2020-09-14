7f886d9ee3890ce18e24ce59d291ecc8ac35e70c
ponzu07 <a@a>
2020-09-14 15:35:09 +0900

    test

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-  let newUserChannel = newState.voice.channel
-  let oldUserChannel = oldState.voice.channel
-  let channelID = "754891923159777371";
 
-  if(oldUserChannel === undefined && newUserChannel !== undefined) {
-    let channels = newState.guild.channels;
-    client.channels.cache.get(channelID).send(newState.user.username + " がチャンネル 「" + newUserChannel.name + "」 に入室しました。");
-  } else if(newUserChannel === undefined){
-    let channels = oldState.guild.channels;
-    client.channels.cache.get(channelID).send(newState.user.username + " がチャンネル 「" + oldUserChannel.name + "」 から退室しました。");
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  const newUserChannel = newMember.voice.channelID
+  const oldUserChannel = oldMember.voice.channelID
+  const textChannel = message.guild.channels.cache.get('754891923159777371')
+
+  if(newUserChannel === '754891923159777371') {
+    textChannel.send(`${newMember.user.username} (${newMember.id}) has joined the channel`)
+  } else if (oldUserChannel === '754891923159777371' && newUserChannel !== '754891923159777371') {
+    textChannel.send(`${newMember.user.username} (${newMember.id}) has left the channel`)
   }
 })
-
 
 client.login(process.env.BOT_TOKEN);
