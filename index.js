c7b048ee8d839a154631054412f2c7dfc275e555
ponzu07 <a@a>
2020-09-14 16:27:29 +0900

    test

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
 
 
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-  const newUserChannel = newMember.voice.channelID
-  const oldUserChannel = oldMember.voice.channelID
+client.on('voiceStateUpdate', (oldState, newState) => {
+  const newUserChannel = newState.voice.channelID
+  const oldUserChannel = oldState.voice.channelID
   const textChannel = message.guild.channels.cache.get('754891923159777371')
 
   if(!oldState.channelID && newState.channelID === '544882228677705742') {
