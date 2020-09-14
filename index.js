d275b13fcc0f5a13f0a07607328077f4d71fa26f
ponzu07 <a@a>
2020-09-15 08:38:10 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
@@ -11,9 +11,9 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   const newUserChannel = newState.channelID
   const oldUserChannel = oldState.channelID
   if(!oldUserChannel){ //vc参加時
-    client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が参加しました。")
+    client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が通話に参加しました。")
   }else if(!newUserChannel){ //vc退出時
-    client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が退出しました。")
+    client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が通話を退出しました。")
   }
 })
 
