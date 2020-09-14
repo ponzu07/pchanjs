7c621445ff0edd2153ba941af32661ee15b72fcf
ponzu07 <a@a>
2020-09-14 14:01:14 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
@@ -14,19 +14,10 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
 
-
   if(oldUserChannel === undefined && newUserChannel !== undefined) {
     let channelID;
     if(setchannelID === undefined){
       let channels = newMember.guild.channels;
-      channelLoop:
-      for (let c of channels) {
-        let channelType = c[1].type;
-        if (channelType === "text") {
-          channelID = c[0];
-          break channelLoop;
-        }
-      }
     } else {
       channelID = setchannelID;
     }
@@ -36,14 +27,6 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     let channelID;
     if(setchannelID === undefined){
       let channels = oldMember.guild.channels;
-      channelLoop:
-      for (let c of channels) {
-        let channelType = c[1].type;
-        if (channelType === "text") {
-          channelID = c[0];
-          break channelLoop;
-        }
-      }
     } else {
       channelID = setchannelID;
     }
