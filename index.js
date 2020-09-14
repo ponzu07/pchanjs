6c2a297e09f91e62487edc443f41ceadacb5889c
ponzu07 <a@a>
2020-09-14 16:20:26 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
@@ -14,10 +14,10 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   const oldUserChannel = oldMember.voice.channelID
   const textChannel = message.guild.channels.cache.get('754891923159777371')
 
-  if(newUserChannel === '544882228677705742') {
-    textChannel.send(`${newMember.user.username} (${newMember.id}) has joined the channel`)
+  if(!oldState.channelID && newState.channelID === '544882228677705742') {
+    textChannel.send(`${newMember.user.username} (${newMember.id}) が通話に参加しました。`)
   } else if (oldUserChannel === '544882228677705742' && newUserChannel !== '544882228677705742') {
-    textChannel.send(`${newMember.user.username} (${newMember.id}) has left the channel`)
+    textChannel.send(`${newMember.user.username} (${newMember.id}) が通話を退出しました。`)
   }
 })
 
