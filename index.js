bdab3971ee53bb6e469e166843008040686fb1f4
ponzu07 <a@a>
2020-09-14 16:01:56 +0900

    test

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
 
 
 
-client.on('voiceStateUpdate', (oldState, newState) => {
-  let newUserChannel = newState.guildMember.voice.channel
-  let oldUserChannel = oldState.guildMember.voice.channel
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  const newUserChannel = newMember.voice.channelID
+  const oldUserChannel = oldMember.voice.channelID
+  const textChannel = message.guild.channels.cache.get('754891923159777371')
 
-  if(oldUserChannel === null && newUserChannel !== null) {
-     // User Joins a voice channel
-    console.log('join');
-
-  } else if(newUserChannel === null){
-    // User leaves a voice channel
-    console.log('leave');
-
+  if(newUserChannel === '544882228677705742') {
+    textChannel.send(`${newMember.user.username} (${newMember.id}) has joined the channel`)
+  } else if (oldUserChannel === '544882228677705742' && newUserChannel !== '544882228677705742') {
+    textChannel.send(`${newMember.user.username} (${newMember.id}) has left the channel`)
   }
 })
 
