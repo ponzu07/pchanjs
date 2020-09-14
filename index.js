3047e661b692c14a9ecef86e30531df818abc020
ponzu07 <a@a>
2020-09-14 10:59:48 +0900

    test

--- index.js
+++ index.js
@@ -9,31 +9,13 @@ client.on('ready', () => {
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
-
+  let channelID = 709227262251106344
 
   if(oldUserChannel === undefined && newUserChannel !== undefined) {
-    let channelID;
     let channels = newMember.guild.channels;
-    channelLoop:
-    for (let c of channels) {
-      let channelType = c[1].type;
-      if (channelType === "text") {
-        channelID = 709227262251106344;
-        break channelLoop;
-      }
-    }
     client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + newUserChannel.name + "」 に入室したよ。");
   } else if(newUserChannel === undefined){
-    let channelID;
     let channels = oldMember.guild.channels;
-    channelLoop:
-    for (let c of channels) {
-      let channelType = c[1].type;
-      if (channelType === "text") {
-        channelID = 709227262251106344;
-        break channelLoop;
-      }
-    }
     client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + oldUserChannel.name + "」 から退室したよ。");
   }
 })
