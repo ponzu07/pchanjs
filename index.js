1fd960938f11e809a56d2421e88c7c829ef858d5
ponzu07 <a@a>
2020-09-14 10:52:07 +0900

    test

--- index.js
+++ index.js
@@ -2,13 +2,40 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
+	console.log('準備ok!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+
+
+  if(oldUserChannel === undefined && newUserChannel !== undefined) {
+    let channelID;
+    let channels = newMember.guild.channels;
+    channelLoop:
+    for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+        channelID = c[0];
+        break channelLoop;
+      }
+    }
+    client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + newUserChannel.name + "」 に入室したよ。");
+  } else if(newUserChannel === undefined){
+    let channelID;
+    let channels = oldMember.guild.channels;
+    channelLoop:
+    for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+        channelID = c[0];
+        break channelLoop;
+      }
+    }
+    client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + oldUserChannel.name + "」 から退室したよ。");
   }
-});
+})
 
 client.login(process.env.BOT_TOKEN);
