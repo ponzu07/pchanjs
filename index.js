27c507c1a505cec86e0f1391cd107c1484777bf4
ponzu07 <a@a>
2020-09-15 08:27:15 +0900

    fix

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: '@VALORANT' } }); //アクティビティ表示
+  client.user.setPresence({ activity: { name: 'VALORANT' } }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
 
-
-
 client.on('voiceStateUpdate', (oldState, newState) => {
-  let newUserChannel = newState.voice.channel
-  let oldUserChannel = oldState.voice.channel
-
-  if(oldUserChannel === null && newUserChannel !== null) {
+  const newUserChannel = newState.channelID
+  const oldUserChannel = oldState.channelID
+  if(!oldUserChannel){ //vc参加時
     client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が参加しました。")
-  } else if(newUserChannel === null){
+  }else if(!newUserChannel){ //vc退出時
     client.channels.cache.get('754891923159777371').send(newState.member.user.username + " が退出しました。")
   }
-})
+});
+
 
 client.login(process.env.BOT_TOKEN);
