853b9c22e6351c29c2541f501fb28f7aa49f7224
ponzu07 <fuzitanirenti@yahoo.co.jp>
2021-03-13 17:32:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('message', msg => {
 
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'Among Us' } }); //アクティビティ表示
+  client.user.setActivity('VALORANT', { type: 'STREAMING' }); //アクティビティ表示
 	console.log('準備ok!');
 });
 
